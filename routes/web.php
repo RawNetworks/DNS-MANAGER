010c612fdabaca4d6d2ddcea545afe3611647fb1
RAW Networks <privat@janhill.email>
2026-01-01 18:43:21 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', function () {
+    return route('auth');
 });
+
+Route::prefix('auth')->group(function () {
+    if (!Auth::User()) {
+        return redirect()->route('login');
+    } else {
+        return redirect()->route('register');
+    }
+    // Auth Routes
+    Route::get('login/', [LoginController::class, 'login'])->name('login');
+    Route::get('register/', [LoginController::class, 'login'])->name('register');
+})->name('auth');
