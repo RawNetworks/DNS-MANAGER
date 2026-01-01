051d114b529dd09b9c2f0dba46ef45632a8a13e0
RAW Networks <privat@janhill.email>
2026-01-01 18:56:20 +0100

    update

---
+++ resources/views/app/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DNS Manager</title>
+
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+</head>
+
+<body>
+
+<h1>Welcome to DNS Manager</h1>
+
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.bundle.js')}}"></script>
+</body>
+
+</html>
