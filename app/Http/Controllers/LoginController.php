ae18714e5157b4a0e19de5b3736af80dd31e6e6d
RAW Networks <privat@janhill.email>
2026-01-01 20:13:25 +0100

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controller;
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
@@ -8,9 +10,11 @@ use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('app.login');
     }
+
     public function login(Request $request)
     {
         $credentials = $request->validate([
@@ -28,7 +32,6 @@ class LoginController extends Controller
         }
 
         if (Auth::attempt($credentials, $request->boolean('remember'))) {
-
             RateLimiter::clear($throttleKey);
             $request->session()->regenerate();
 
