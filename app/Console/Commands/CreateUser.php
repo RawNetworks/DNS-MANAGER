e2d27e858226c18d3e5602aa9504fe294e71e98a
RAW Networks <privat@janhill.email>
2026-01-01 19:17:24 +0100

    add command

--- app/Console/Commands/CreateUser.php
+++ app/Console/Commands/CreateUser.php
@@ -10,21 +10,34 @@ class CreateUser extends Command
      * The name and signature of the console command.
      *
      * @var string
-     */
-    protected $signature = 'app:create-user';
-
     /**
-     * The console command description.
-     *
-     * @var string
+     * Die Signatur wurde um {role} erweitert
      */
-    protected $description = 'Command description';
+    protected $signature = 'dns:create-user {username} {email} {password} {role=user}';
 
-    /**
-     * Execute the console command.
-     */
+    protected $description = 'Make a DNS user with a specific role, ROLES: admin or by default user';
+
     public function handle()
     {
-        //
+        $username = $this->argument('username');
+        $email    = $this->argument('email');
+        $password = $this->argument('password');
+        $role     = $this->argument('role');
+
+        $this->info("Erstelle Benutzer: {$username}...");
+
+        try {
+            $user = \App\Models\User::create([
+                'name'     => $username,
+                'email'    => $email,
+                'password' => \Illuminate\Support\Facades\Hash::make($password),
+                'role'     => $role,
+            ]);
+
+            $this->success("Benutzer {$username} wurde erfolgreich als '{$role}' angelegt!");
+
+        } catch (\Exception $e) {
+            $this->error("Fehler beim Erstellen: " . $e->getMessage());
+        }
     }
 }
